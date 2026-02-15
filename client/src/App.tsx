a7c16fb3625b5fe40514dcbad9f29540a080922d
ariealdonny <dianadanapaylater@gmail.com>
2026-02-15 20:01:49 +0800

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import FundTransfer from "@/pages/fund-transfer";
+import Authorization from "@/pages/authorization";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={FundTransfer} />
+      <Route path="/authorize/:id" component={Authorization} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
